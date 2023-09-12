570e930677fa07696e0739d6658ddc863ad5ee7f
houston <houston@mail.com>
2023-09-12 13:31:42 +0300

    try3

--- scripts/item_list.ts
+++ scripts/item_list.ts
@@ -6,3 +6,61 @@ type ItemNft = {
 
 export const ITEMS_URL: ItemNft[] = [
 ];
+
+for(let i = 0; i<13; i++){
+    ITEMS_URL.push({
+        content:'https://www.anncom.ru/dialer/nft/nft_item.json',
+        ch_content: 'https://www.anncom.ru/dialer/nft/nft_iHOLD/nft_item_open_vip.json',
+        type: 'vip',
+    })
+}
+
+for(let i = 0; i<5; i++){
+    ITEMS_URL.push({
+        content:'https://www.anncom.ru/dialer/nft/nft_item.json',
+        ch_content: 'https://www.anncom.ru/dialer/nft/nft_iHOLD/nft_item_open_shirt.json',
+        type: 'shirt',
+    })
+}
+
+
+for(let i = 0; i<2; i++){
+    ITEMS_URL.push({
+        content:'https://www.anncom.ru/dialer/nft/nft_item.json',
+        ch_content: 'https://www.anncom.ru/dialer/nft/nft_iHOLD/nft_item_open_cap.json',
+        type: 'cap',
+    })
+}
+
+
+for(let i = 0; i<2; i++){
+    ITEMS_URL.push({
+        content:'https://www.anncom.ru/dialer/nft/nft_item.json',
+        ch_content: 'https://www.anncom.ru/dialer/nft/nft_iHOLD/nft_item_open_dinner.json',
+        type: 'dinner',
+    })
+}
+
+for(let i = 0; i<2; i++){
+    ITEMS_URL.push({
+        content:'https://www.anncom.ru/dialer/nft/nft_item.json',
+        ch_content: 'https://www.anncom.ru/dialer/nft/nft_iHOLD/nft_item_open_film_set.json',
+        type: 'film',
+    })
+}
+
+for(let i = 0; i<2; i++){
+    ITEMS_URL.push({
+        content:'https://www.anncom.ru/dialer/nft/nft_item.json',
+        ch_content: 'https://www.anncom.ru/dialer/nft/nft_iHOLD/nft_item_open_integration.json',
+        type: 'integration',
+    })
+}
+
+for(let i = 0; i<2; i++){
+    ITEMS_URL.push({
+        content:'https://www.anncom.ru/dialer/nft/nft_item.json',
+        ch_content: 'https://www.anncom.ru/dialer/nft/nft_iHOLD/nft_item_open_theater.json',
+        type: 'theater',
+    })
+}
